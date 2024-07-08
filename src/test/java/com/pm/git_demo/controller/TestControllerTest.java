bceff13d10310eee15fbf41791be30eeb57efb98
phonemyintmar <106571217+phonemyintmar@users.noreply.github.com>
2024-07-08 08:46:58 +0700

    Release v1.0  merge
    * Add Controller
    
    * Update controller (#2)
    
    * Update controller
    
    * Update url
    
    * Add Unit test
    
    ---------
    
    Co-authored-by: PM Laptop <PM@home>
    
    ---------
    
    Co-authored-by: PM Laptop <PM@home>

---
+++ src/test/java/com/pm/git_demo/controller/TestControllerTest.java
@@ -0,0 +1,16 @@
+package com.pm.git_demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestControllerTest {
+
+    @Test
+    public void test() {
+        var x = 1;
+        var y = 2;
+        assertNotEquals(x, y);
+    }
+
+}
