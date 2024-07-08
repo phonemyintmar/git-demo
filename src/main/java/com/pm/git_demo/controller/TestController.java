fc035ca9903f3c063f2078268c099c0b3f9af76f
phonemyintmar <106571217+phonemyintmar@users.noreply.github.com>
2024-07-08 08:42:01 +0700

    Update controller (#2)
    * Update controller
    
    * Update url
    
    * Add Unit test
    
    ---------
    
    Co-authored-by: PM Laptop <PM@home>

--- src/main/java/com/pm/git_demo/controller/TestController.java
+++ src/main/java/com/pm/git_demo/controller/TestController.java
@@ -1,4 +1,13 @@
 package com.pm.git_demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping("ping")
+    public String ping(){
+        return "pong";
+    }
 }
